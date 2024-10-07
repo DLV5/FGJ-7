f4a24732f9ef6f1f0004388c42dd4427157624f0
Kameji <diman567tak@gmail.com>
2024-10-07 15:01:08 +0300

    Add project files.

---
+++ Assets/Scripts/ObjectMover.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+//Moves object when it is not in a field of view
+public class ObjectMover : MonoBehaviour
+{
+    [SerializeField] private Transform _PlayerPosition;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void IsInAFOV()
+    {
+
+    }
+}
